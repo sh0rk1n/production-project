35f8d3a561899219282a0ee9ffa4b6940a48bc54
sh0rk1n <155022624+sh0rk1n@users.noreply.github.com>
2024-06-09 04:43:07 +0300

    Added i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,26 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss';
-import {Link, Route, Routes} from 'react-router-dom'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {NavBar} from "widgets/NavBar";
+import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
 
 
 
 const App = () => {
-    const {theme,toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>loading...</div>}>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage/>}/>
-                <Route path={'/'} element={<MainPage/>}/>
-            </Routes>
+            <Suspense fallback=''>
+            <NavBar/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
             </Suspense>
-
         </div>
     );
 };
