3b1f7dc6ffe84c728b97983df4c48a77ea841f30
sh0rk1n <155022624+sh0rk1n@users.noreply.github.com>
2024-06-19 18:40:40 +0300

    add articles page

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -3,6 +3,7 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import MainIcon from 'widgets/assets/icons/main-20-20.svg';
 import AboutIcon from 'widgets/assets/icons/about-20-20.svg';
 import ProfileIcon from 'widgets/assets/icons/profile-20-20.svg';
+import ArticleIcon from 'widgets/assets/icons/article-20-20.svg';
 
 export interface SidebarItemType {
     path: string
@@ -26,6 +27,12 @@ export const SidebarItemsList: SidebarItemType[] = [
         path: RoutePath.profile,
         Icon: ProfileIcon,
         text: 'Профиль',
+        authOnly: true,
+    },
+    {
+        path: RoutePath.articles,
+        Icon: ArticleIcon,
+        text: 'Cтатьи',
         authOnly: true,
     },
 ];
