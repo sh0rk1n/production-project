0c0aa8fc10b8be814dc45218acd0363e54a50142
sh0rk1n <155022624+sh0rk1n@users.noreply.github.com>
2024-06-25 16:03:24 +0300

    add react virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { HTMLAttributeAnchorTarget, memo } from 'react';
 import { ArticleListItemSkeleton } from 'entities/Article/ui/ArticleListItem/ArticleListItemSkeleton';
 import { Text, TextSize } from 'shared/ui/Text/Text';
+import { List, ListRowProps, WindowScroller } from 'react-virtualized';
+import { PAGE_ID } from 'widgets/Page/Page';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import cls from './ArticleList.module.scss';
 import { Article, ArticleViewType } from '../../model/types/article';
@@ -11,8 +13,8 @@ interface ArticleListProps {
     className?: string;
     articles: Article[]
     isLoading?: boolean;
+    target?: HTMLAttributeAnchorTarget;
     view?: ArticleViewType;
-    target?: HTMLAttributeAnchorTarget
 }
 
 const getSkeletons = (view: ArticleViewType) => new Array(view === ArticleViewType.SMALL ? 9 : 3)
@@ -31,18 +33,40 @@ export const ArticleList = memo((props: ArticleListProps) => {
     } = props;
     const { t } = useTranslation();
 
-    const renderArticle = (article: Article) => {
-        if (!article || !article.id) {
-            return null;
+    const isBig = view === ArticleViewType.BIG;
+
+    const itemsPerRow = isBig ? 1 : 3;
+    const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
+
+    const rowRender = ({
+        index, key, style,
+    }: ListRowProps) => {
+        const items = [];
+        const fromIndex = index * itemsPerRow;
+        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+
+        for (let i = fromIndex; i < toIndex; i += 1) {
+            if (articles[i]) {
+                items.push(
+                    <ArticleListItem
+                        article={articles[i]}
+                        view={view}
+                        target={target}
+                        key={`str${i}`}
+                        className={cls.card}
+                    />,
+                );
+            }
         }
+
         return (
-            <ArticleListItem
-                article={article}
-                view={view}
-                className={cls.card}
-                key={article.id}
-                target={target}
-            />
+            <div
+                key={key}
+                style={style}
+                className={cls.row}
+            >
+                {items}
+            </div>
         );
     };
 
@@ -55,11 +79,35 @@ export const ArticleList = memo((props: ArticleListProps) => {
     }
 
     return (
-        <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-            {articles.length > 0
-                ? articles.map(renderArticle)
-                : null}
-            {isLoading && getSkeletons(view)}
-        </div>
+        <WindowScroller
+            scrollElement={document.getElementById(PAGE_ID) as Element}
+        >
+            {({
+                height,
+                width,
+                registerChild,
+                onChildScroll,
+                isScrolling,
+                scrollTop,
+            }) => (
+                <div
+                    ref={registerChild}
+                    className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+                >
+                    <List
+                        height={height ?? 700}
+                        rowCount={rowCount}
+                        rowHeight={isBig ? 700 : 330}
+                        rowRenderer={rowRender}
+                        width={width ? width - 80 : 700}
+                        autoHeight
+                        onScroll={onChildScroll}
+                        isScrolling={isScrolling}
+                        scrollTop={scrollTop}
+                    />
+                    {isLoading && getSkeletons(view)}
+                </div>
+            )}
+        </WindowScroller>
     );
 });
--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,5 +1,6 @@
-import { HTMLAttributeAnchorTarget, memo, useCallback } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { HTMLAttributeAnchorTarget, memo, useCallback } from 'react';
 import { Text } from 'shared/ui/Text/Text';
 import { Icon } from 'shared/ui/Icon/Icon';
 import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
@@ -9,23 +10,24 @@ import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useNavigate } from 'react-router-dom';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 import cls from './ArticleListItem.module.scss';
 import {
     Article, ArticleBlockType, ArticleTextBlock, ArticleViewType,
 } from '../../model/types/article';
+import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 
 interface ArticleListItemProps {
     className?: string;
-    article:Article
-    view:ArticleViewType
-    target?: HTMLAttributeAnchorTarget
+    article: Article;
+    view: ArticleViewType;
+    target?: HTMLAttributeAnchorTarget;
 }
 
 export const ArticleListItem = memo((props: ArticleListItemProps) => {
     const {
         className, article, view, target,
     } = props;
+    const { t } = useTranslation();
 
     const types = <Text text={article.type.join(', ')} className={cls.types} />;
     const views = (
@@ -39,6 +41,7 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
         const textBlock = article.blocks.find(
             (block) => block.type === ArticleBlockType.TEXT,
         ) as ArticleTextBlock;
+
         return (
             <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
                 <Card className={cls.card}>
@@ -58,10 +61,8 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
                             target={target}
                             to={RoutePath.article_details + article.id}
                         >
-                            <Button
-                                theme={ButtonTheme.OUTLINE}
-                            >
-                                Читать далее...
+                            <Button theme={ButtonTheme.OUTLINE}>
+                                {t('Читать далее...')}
                             </Button>
                         </AppLink>
                         {views}
@@ -77,9 +78,9 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
             to={RoutePath.article_details + article.id}
             className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}
         >
-            <Card>
-                <div className={cls.imgWraper}>
-                    <img src={article.img} className={cls.img} alt={article.title} />
+            <Card className={cls.card}>
+                <div className={cls.imageWrapper}>
+                    <img alt={article.title} src={article.img} className={cls.img} />
                     <Text text={article.createdAt} className={cls.date} />
                 </div>
                 <div className={cls.infoWrapper}>
